c62a55e2fff06501758e685bbafeaab9a4815348
Joy Wanjiru <wanjirusimiyu@gmail.com>
2024-05-03 03:17:02 +0300

    Update code-along lab

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+   const [is0ff, setIsOff]= useState(true);
+
+   function handleClick(){
+    setIsOff((is0ff) => !is0ff);
+   }
+
+   const color = is0ff ? "white" : "red";
+
+  return(
+    <button style={{ background: color }} onClick={handleClick}>
+      {is0ff ? "OFF" : "ON"}</button>
+  ) 
 }
 
 export default Toggle;
+
+
+
